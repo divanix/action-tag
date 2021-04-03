2576f8cc590ec8a94f2fe7a271b46a2dfdc91fbd
igibek <koishybayev.igibek@gmail.com>
2021-04-02 20:51:20 -0400

    comment

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ try {
   const token = transform(core.getInput('token'));
   console.log('Version 1.2.3');
   console.log('Fixed:', token);
+  //
 } catch (error) {
   core.setFailed(error.message);
 }
