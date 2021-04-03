3d9fdd511eaa763a6cf7d65d0b4c4b05fdac3229
igibek <koishybayev.igibek@gmail.com>
2021-04-02 20:53:03 -0400

    latest

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
   const token = transform(core.getInput('token'));
   console.log('Version 1.2.3');
   console.log('Fixed:', token);
-  //
+  console.log('Latest from main');
 } catch (error) {
   core.setFailed(error.message);
 }
