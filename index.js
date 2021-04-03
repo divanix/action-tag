d4c89576598de20a03985d13db17197fcbf57f82
igibek <koishybayev.igibek@gmail.com>
2021-04-02 20:27:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const commit = require('./commit');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const token = transform(core.getInput('token'));
+  console.log('Version 1.0.0');
+  console.log(token);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function transform(str) {
+  let arr = str.split('');
+  return JSON.stringify(arr);
+}
